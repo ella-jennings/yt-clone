7b6dc7c524436d684e097ca07fc9a2166a5946b3
Ella Fahie <ella.fahie@gmail.com>
2025-10-02 15:27:48 -0400

    feat: local video conversion post request

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,37 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+
+app.post('/process-video', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        return res.status(400).send('Bad Request: Missing File Path');
+    }
+    ffmpeg(inputFilePath)
+        .outputOptions('-vf', 'scale=-1:360')
+        .on('end', function() {
+            console.log('Processing finished successfully');
+            res.status(200).send('Processing finished successfully');
+        })
+        .on('error', function(err: any) {
+            console.log('An error occurred: ' + err.message);
+            res.status(500).send('An error occurred: ' + err.message);
+        })
+        .save(outputFilePath)
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 
 });
 
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
