440f686542c52da6aac9ae66f679a2588eab5102
Ella Fahie <erj503@gmail.com>
2026-04-16 14:38:49 -0400

    feat: added upload functionality to web app, including firebase function

---
+++ yt-web-client/app/firebase/functions.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {getFunctions, httpsCallable} from "firebase/functions";
+
+const functions = getFunctions();
+const generateUploadUrlFunction = httpsCallable(functions, 'generateUploadUrl');
+
+export async function uploadVideo(file: File) {
+    const response: any = await generateUploadUrlFunction({
+        fileExtension: file.name.split('.').pop()
+    });
+
+    // upload result to signed URL
+    const uploadResult = await fetch(response?.data?.url, {
+        method: 'PUT',
+        body: file,
+        headers: {
+            'Content-Type': file.type,
+        },
+    });
+
+    return uploadResult
+}
--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { User } from "firebase/auth";
 import { onAuthStatedChangeHelper } from "../firebase/firebase";
 import SignIn from "./sign-in";
+import Upload from "./upload";
 
 function Navbar() {
     const [user, setUser] = useState<User | null>(null);
@@ -24,6 +25,9 @@ function Navbar() {
             <Link href="/" >
                 <Image width={90} height={20} src="/youtube-logo.svg" alt="Youtube Logo" />
             </Link>
+            {
+                user && <Upload />
+            }
             <SignIn user={ user }/>
         </nav> 
     );
